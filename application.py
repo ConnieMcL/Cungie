ba0750ccb07ac80b2e75486529d6f8cf049c5ce9
ConnieMcLaughlin-L00162930 <L00162930@student.lyit.ie>
2022-12-08 21:25:49 +0000

    Another try

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from website import create_app
 
-app = create_app()
+application = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    application.run(debug=False)
--- website/__init__.py
+++ website/__init__.py
@@ -8,28 +8,28 @@ DB_NAME = "database.db"
 
 
 def create_app():
-    application = app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'BEANSAREGREAT yuyumyumyum'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    application = Flask(__name__)
+    application.config['SECRET_KEY'] = 'BEANSAREGREAT yuyumyumyum'
+    application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(application)
 
 
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    application.register_blueprint(views, url_prefix='/')
+    application.register_blueprint(auth, url_prefix='/')
 
 
     from .models import User, Note
 
-    with app.app_context():
+    with application.app_context():
         db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+    login_manager.init_app(application)
     
 
     @login_manager.user_loader
@@ -38,7 +38,7 @@ def create_app():
 
 
 
-    return app
+    return application
 
 #def create_database(app):
 #    if not path.exists('website/' + DB_NAME):
